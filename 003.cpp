2667b850c110819d367f31756eeef1b284b941f4
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-09 15:24:20 +0300

    Create 003.cpp

---
+++ 003.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    float a, c;
+    char b;
+    getline(cin, str);
+    istringstream stream(str);
+    stream >> c;
+    while (stream >> b)
+    {
+        if (b == '+')
+        {
+            stream >> a;
+            c = c + a;
+        }
+        else if (b == '-')
+        {
+            stream >> a;
+            c = c - a;
+        }
+        else if (b == '*')
+        {
+            stream >> a;
+            c = c * a;
+        }
+        else if (b == '/')
+        {
+            stream >> a;
+            c = c / a;
+        }
+
+    }
+    cout << c;
+    return 0;
+
+}
