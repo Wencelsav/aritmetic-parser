c43d81d30a2d53ddce32c00909afa6846b307c77
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-12 01:04:47 +0300

    Update 003.cpp

--- 003.cpp
+++ 003.cpp
@@ -1,41 +1,135 @@
 #include <iostream>
-#include <sstream>
 
 using namespace std;
 
-int main()
-{
-    string str;
-    float a, c;
-    char b;
-    getline(cin, str);
-    istringstream stream(str);
-    stream >> c;
-    while (stream >> b)
-    {
-        if (b == '+')
-        {
-            stream >> a;
-            c = c + a;
-        }
-        else if (b == '-')
-        {
-            stream >> a;
-            c = c - a;
-        }
-        else if (b == '*')
-        {
-            stream >> a;
-            c = c * a;
-        }
-        else if (b == '/')
-        {
-            stream >> a;
-            c = c / a;
-        }
 
-    }
-    cout << c;
-    return 0;
+
+
+double number()
+
+{
+
+	int a = 0;
+	for (;;)
+
+	{
+
+		char c = cin.get();
+		if ((c >= '0') && (c <= '9'))
+
+		{
+			a = a * 10 + (c - '0');
+		}
+		else
+
+		{
+
+			cin.putback(c);
+
+			return a;
+
+		}
+
+	}
+
+}
+
+
+double multdiv()
+
+{
+
+	double x = number();
+
+	for (;;)
+
+	{
+
+		char c = cin.get();
+
+		switch (c)
+
+		{
+
+		case '*':
+
+			x = x*number();
+
+			break;
+
+		case '/':
+
+			x = x / number();
+
+			break;
+
+		default:
+
+			cin.putback(c);
+
+			return x;
+		}
+
+	}
+
+}
+
+
+
+
+double addsub()
+
+{
+
+	double x = multdiv();
+	for (;;)
+
+	{
+
+		char c = cin.get();
+
+		switch (c)
+
+		{
+
+		case '+':
+
+			x = x + multdiv();
+
+			break;
+
+		case '-':
+
+			x = x - multdiv();
+
+			break;
+
+		default:
+
+			cin.putback(c);
+
+			return x;
+
+		}
+
+	}
+
+}
+
+
+
+
+
+
+
+int main()
+
+{
+
+	double a = addsub();
+
+	cout << a << '\n';
+
+
 
 }
